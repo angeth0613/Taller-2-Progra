253f5f217fdfee25e4c246d715f0f3e85cff2d41
angeth0613 <angeth0613@gmail.com>
2026-04-29 09:44:42 -0500

    clases taller2

--- src/co/edu/uptc/enums/IdentificationType.java
+++ src/co/edu/uptc/enums/IdentificationType.java
@@ -1,5 +1,17 @@
 package co.edu.uptc.enums;
 
 public enum IdentificationType {
+    CC("Cédula de ciudadanía"),
+    TI("Tarjeta de identidad"),
+    CE("Cédula de extranjería"),
+    PA("Pasaporte");
 
+    private String nombre;
+    
+    
+    
+
+    IdentificationType(String nombre) {
+        this.nombre = nombre;
+    }
 }
